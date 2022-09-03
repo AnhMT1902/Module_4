dc0bd72fb8f87c6993d3013052e61ab904d4bff8
tminh1902 <minhanh190202@gmail.com>
2022-09-03 15:38:07 +0700

    done 5th

---
+++ MĐ2/1. Tổng quan về ES6/arrow-functions-example.js
@@ -0,0 +1,11 @@
+let arr = [2,4,1,5,6,2];
+let square = (item) => item * item;
+function arraySq(func, arr) {
+    let newArray = [];
+    arr.forEach((element) => {
+        newArray.push(func(element));
+    })
+    return newArray;
+}
+
+console.log(arraySq(square, arr));
