d81ac121050f26afbfcf47752fbc6e2e3cfaccf1
tminh1902 <minhanh190202@gmail.com>
2022-09-10 12:51:50 +0700

    done

--- MĐ2/AccessModifier-StaticMethod-StaiticProperty/Dem-so-luong-doi-tuong-cua-mot-lop.ts
+++ MĐ2/AccessModifier-StaticMethod-StaiticProperty/Dem-so-luong-doi-tuong-cua-mot-lop.ts
@@ -8,7 +8,7 @@ class Application {
 }
 
 console.log(Application.count);
-let app1 = new Application("minh anh");
+let app1 = new Application("minhanh");
 console.log(Application.count);
 let app2 = new Application("minh anh");
 console.log(Application.count);
